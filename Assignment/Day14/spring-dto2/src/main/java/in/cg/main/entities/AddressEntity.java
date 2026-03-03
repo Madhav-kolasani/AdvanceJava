6f84878f4a623127d36bc874c9de812d78aff75b
Madhav <madhavkolasani@gmail.com>
2026-03-03 17:20:34 +0530

    DTO with relations of Entities

---
+++ Assignment/Day14/spring-dto2/src/main/java/in/cg/main/entities/AddressEntity.java
@@ -0,0 +1,57 @@
+package in.cg.main.entities;
+
+import jakarta.persistence.*;
+
+@Entity
+public class AddressEntity {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	private Long id;
+
+	private String houseNo;
+	private String street;
+	private String city;
+	private String pincode;
+
+	public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getHouseNo() {
+		return houseNo;
+	}
+
+	public void setHouseNo(String houseNo) {
+		this.houseNo = houseNo;
+	}
+
+	public String getStreet() {
+		return street;
+	}
+
+	public void setStreet(String street) {
+		this.street = street;
+	}
+
+	public String getCity() {
+		return city;
+	}
+
+	public void setCity(String city) {
+		this.city = city;
+	}
+
+	public String getPincode() {
+		return pincode;
+	}
+
+	public void setPincode(String pincode) {
+		this.pincode = pincode;
+	}
+
+}
